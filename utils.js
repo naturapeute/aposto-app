1b4ee8c8a4a86d013f2d027af7535f7551ce69ed
mlbiche <tmlb@protonmail.com>
2020-02-18 10:12:07 +0100

    Fix receipt generation
    - Fix receipt generation
    - Move getDateString function to utils

--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
 function $(query) {
     return document.querySelector(query);
 }
+
+function getDateString(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() +1).padStart(2, '0');
+
+    return `${day}.${month}.${date.getFullYear()}`;
+}
