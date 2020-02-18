c67d66f33d35e79096b9b65366b6ee4b1c90c4f9
Mlbiche <tmlb@protonmail.com>
2020-02-18 10:24:55 +0100

    Update indents

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 function $(query) {
-    return document.querySelector(query);
+  return document.querySelector(query);
 }
 
 function getDateString(date) {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() +1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
 
-    return `${day}.${month}.${date.getFullYear()}`;
+  return `${day}.${month}.${date.getFullYear()}`;
 }
