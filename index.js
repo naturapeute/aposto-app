cf064c9643c20668e4d2052d5eaa1a3689e8756a
Mlbiche <tmlb@protonmail.com>
2020-02-27 16:47:40 +0100

    Move matomo loader

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+loadMatomo()
+
 let isInfoEdited = false
 let customers
 
@@ -254,4 +256,17 @@ function dateObjectToDateInput(date) {
   const month = String(date.getMonth() + 1).padStart(2, '0')
 
   return `${date.getFullYear()}-${month}-${day}`
+}
+
+function loadMatomo() {
+  var _paq = window._paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function () {
+    var u = "//stats.anonym.dev/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '5']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+  })();
 }
