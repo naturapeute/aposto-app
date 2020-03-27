c020c9b113e139f2ba37c81c5c2f179722cfa41b
Mlbiche <tmlb@protonmail.com>
2020-03-27 18:20:03 +0100

    Install Material theme and button component
    - Install @material/theme
    - Install @material/button for Button component
    - Install sass for using Material SCSS stylesheet on blunding
    - Install postcss to use global keyword on App SCSS stylesheet
    - Install svelte-preprocess to use sass and postcss on bundlisation
    - Add svelte-preprocess auto-preprocess to Rollup bundlisation

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
+import autoPreprocess from 'svelte-preprocess'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -22,7 +23,12 @@ export default {
       // a separate file - better for performance
       css: css => {
         css.write('public/build/bundle.css')
-      }
+      },
+      preprocess: autoPreprocess({
+        includePaths: [
+          './node_modules'
+        ]
+      })
     }),
 
     // If you have external dependencies installed from
