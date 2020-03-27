e5780b284241fc539f211dfc95bb07228b0cfde3
Mlbiche <tmlb@protonmail.com>
2020-03-27 18:22:03 +0100

    Implement Button Material-Svelte component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-  export let name
+  import Button from './components/atoms/Button/Button.svelte'
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello world!</h1>
+  <Button unelevated>Material Button</Button>
 </main>
 
 <style src="./App.scss" global></style>
---
+++ src/components/atoms/Button/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let raised = false // eslint-disable-line prefer-const
+  export let unelevated = false // eslint-disable-line prefer-const
+  export let outlined = false // eslint-disable-line prefer-const
+</script>
+
+<button
+  class="
+    mdc-button
+    {raised ? 'mdc-button--raised' : ''}
+    {unelevated ? 'mdc-button--unelevated' : ''}
+    {outlined ? 'mdc-button--outlined' : ''}
+  ">
+  <div class="mdc-button__ripple"></div>
+  <span class="mdc-button__label">
+    <slot></slot>
+  </span>
+</button>
