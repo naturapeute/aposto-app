1dbaadc257c42c0f4a49455b6b0c2272e3644a24
Mlbiche <tmlb@protonmail.com>
2020-03-30 17:11:21 +0200

    Implement first draft of Material TextField component

---
+++ src/components/TextField/TextField.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { MDCTextField } from '@material/textfield'
+  import { onMount, onDestroy } from 'svelte'
+
+  export let fieldId
+
+  let textField
+  let thisMDCTextField
+
+  onMount(() => {
+    thisMDCTextField = new MDCTextField(textField)
+  })
+
+  onDestroy(() => {
+    if (thisMDCTextField) thisMDCTextField.destroy()
+  })
+</script>
+
+<label bind:this={textField} class="mdc-text-field mdc-text-field--outlined">
+  <input type="text" class="mdc-text-field__input" aria-labelledby={fieldId}>
+  <div class="mdc-notched-outline">
+    <div class="mdc-notched-outline__leading"></div>
+    <div class="mdc-notched-outline__notch">
+      <span class="mdc-floating-label" id={fieldId}><slot></slot></span>
+    </div>
+    <div class="mdc-notched-outline__trailing"></div>
+  </div>
+</label>
+
+<style src="./TextField.scss"></style>
