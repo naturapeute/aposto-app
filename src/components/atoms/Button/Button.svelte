c130fb8e84bc4a7191d969414eee01e00e507550
mlbiche <tmlb@protonmail.com>
2020-03-30 15:33:02 +0200

    Dispatch click event on Button component
    - Dispatch click event on Button component
    - Improve ESLint to ignore event handler starting with `on` and unused variable _

--- src/components/atoms/Button/Button.svelte
+++ src/components/atoms/Button/Button.svelte
@@ -1,7 +1,16 @@
 <script>
+  import { createEventDispatcher } from 'svelte'
+
   export let raised = false // eslint-disable-line prefer-const
   export let unelevated = false // eslint-disable-line prefer-const
   export let outlined = false // eslint-disable-line prefer-const
+  export let icon
+
+  const dispatch = createEventDispatcher()
+
+  function onClick(event) {
+    dispatch('click', event)
+  }
 </script>
 
 <button
@@ -10,7 +19,8 @@
     {raised ? 'mdc-button--raised' : ''}
     {unelevated ? 'mdc-button--unelevated' : ''}
     {outlined ? 'mdc-button--outlined' : ''}
-  ">
+  "
+  on:click={onClick}>
   <div class="mdc-button__ripple"></div>
   <span class="mdc-button__label">
     <slot></slot>
