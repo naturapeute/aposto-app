afafc8b239a734d491ea4aedd707f9363093a5c1
mlbiche <tmlb@protonmail.com>
2020-03-30 15:34:09 +0200

    Implement Username component

---
+++ src/components/atoms/Username/Username.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  import Button from '../Button/Button.svelte'
+
+  export let username
+
+  const dispatch = createEventDispatcher()
+
+  function onOpenUserPannel() {
+    dispatch('openUserPannel', {})
+  }
+</script>
+
+<Button icon="account_circle" on:click={onOpenUserPannel}>{username}</Button>
