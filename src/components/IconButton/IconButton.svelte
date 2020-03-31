23e959b809c8d08b0618f767fc1943a3a3e73c3d
mlbiche <tmlb@protonmail.com>
2020-03-31 16:34:50 +0200

    Implement IconButton component

---
+++ src/components/IconButton/IconButton.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  export let icon
+
+  const dispatch = createEventDispatcher()
+
+  const onClick = e => {
+    dispatch('click', e)
+  }
+</script>
+
+<button class="mdc-icon-button material-icons" on:click={onClick}>{icon}</button>
