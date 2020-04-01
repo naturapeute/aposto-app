88aa63fa2793ade4002d261364fc6fff1f87ca14
mlbiche <tmlb@protonmail.com>
2020-04-01 11:14:33 +0200

    Improve UserPannel form margins

--- src/components/UserPanel/UserPanel.svelte
+++ src/components/UserPanel/UserPanel.svelte
@@ -51,23 +51,27 @@
       <form class="aposto-form" on:submit|preventDefault={onSubmit}>
         <div class="user-panel-content">
           <h2 class="mdc-typography--overline">Auteur des factures</h2>
-          <TextField bind:value={_author.name} fieldId="author-name">Nom ou entreprise</TextField>
-          <TextField bind:value={_author.street} fieldId="author-street">Rue et n°</TextField>
-          <TextField bind:value={_author.NPA} fieldId="author-zip">NPA</TextField>
-          <TextField bind:value={_author.city} fieldId="author-city">Localité</TextField>
-          <TextField bind:value={_author.email} type="email" fieldId="author-email">Email</TextField>
-          <TextField bind:value={_author.phone} type="tel" fieldId="author-phone">Téléphone</TextField>
-          <TextField bind:value={_author.RCC} type="tel" fieldId="author-rcc">N°RCC</TextField>
-          <TextField bind:value={_author.GLN} type="tel" fieldId="author-gln">N°GLN</TextField>
+          <div class="aposto-form-section">
+            <TextField bind:value={_author.name} fieldId="author-name">Nom ou entreprise</TextField>
+            <TextField bind:value={_author.street} fieldId="author-street">Rue et n°</TextField>
+            <TextField bind:value={_author.NPA} fieldId="author-zip">NPA</TextField>
+            <TextField bind:value={_author.city} fieldId="author-city">Localité</TextField>
+            <TextField bind:value={_author.email} type="email" fieldId="author-email">Email</TextField>
+            <TextField bind:value={_author.phone} type="tel" fieldId="author-phone">Téléphone</TextField>
+            <TextField bind:value={_author.RCC} type="tel" fieldId="author-rcc">N°RCC</TextField>
+            <TextField bind:value={_author.GLN} type="tel" fieldId="author-gln">N°GLN</TextField>
+          </div>
           <h2 class="mdc-typography--overline">Thérapeute</h2>
-          <TextField bind:value={_therapist.firstName} fieldId="therapist-name">Nom ou entreprise</TextField>
-          <TextField bind:value={_therapist.lastName} fieldId="therapist-name">Nom ou entreprise</TextField>
-          <TextField bind:value={_therapist.street} fieldId="therapist-street">Rue et n°</TextField>
-          <TextField bind:value={_therapist.NPA} fieldId="therapist-zip">NPA</TextField>
-          <TextField bind:value={_therapist.city} fieldId="therapist-city">Localité</TextField>
-          <TextField bind:value={_therapist.phone} type="tel" fieldId="therapist-phone">Téléphone</TextField>
-          <TextField bind:value={_therapist.RCC} type="tel" fieldId="therapist-rcc">N°RCC</TextField>
-          <TextField bind:value={_therapist.GLN} type="tel" fieldId="therapist-gln">N°GLN</TextField>
+          <div class="aposto-form-section">
+            <TextField bind:value={_therapist.firstName} fieldId="therapist-name">Nom ou entreprise</TextField>
+            <TextField bind:value={_therapist.lastName} fieldId="therapist-name">Nom ou entreprise</TextField>
+            <TextField bind:value={_therapist.street} fieldId="therapist-street">Rue et n°</TextField>
+            <TextField bind:value={_therapist.NPA} fieldId="therapist-zip">NPA</TextField>
+            <TextField bind:value={_therapist.city} fieldId="therapist-city">Localité</TextField>
+            <TextField bind:value={_therapist.phone} type="tel" fieldId="therapist-phone">Téléphone</TextField>
+            <TextField bind:value={_therapist.RCC} type="tel" fieldId="therapist-rcc">N°RCC</TextField>
+            <TextField bind:value={_therapist.GLN} type="tel" fieldId="therapist-gln">N°GLN</TextField>
+          </div>
           <!--
             NOTE : As Firefox and IE does not properly handle padding-bottom on overflow: scroll containers,
             a padding-like container is added.
