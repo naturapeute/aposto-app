1b965b9d6c19502c5580c4a3178b363b6ad5d716
mlbiche <tmlb@protonmail.com>
2020-04-02 14:11:03 +0200

    Add PatientView, header and UserPanel to App
    - Add Patient View to App
    - Design App main
    - Add and design header
    - Add UserPanel

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,35 @@
 <script>
-  import Button from './components/atoms/Button/Button.svelte'
+  // eslint-disable-next-line no-unused-vars
+  import { authorFixture, therapistFixture, patientsFixture } from './fixtures.mjs'
+  import Username from './components/Username/Username.svelte'
+  import TextField from './components/TextField/TextField.svelte'
+  import UserPanel from './components/UserPanel/UserPanel.svelte'
+  import PatientView from './components/PatientView/PatientView.svelte'
+
+  let author = { ...authorFixture } // eslint-disable-line no-unused-vars
+  let therapist = { ...therapistFixture } // eslint-disable-line no-unused-vars
+  const patients = [...patientsFixture]
+  let userPanelOpenned = false // eslint-disable-line no-unused-vars
+  const selectedPatient = patients[0] // eslint-disable-line no-unused-vars
+
+  const onToggleUserPanel = () => {
+    userPanelOpenned = !userPanelOpenned
+  }
+
+  const onUpdateUser = e => {
+    author = { ...e.detail.author }
+    therapist = { ...e.detail.therapist }
+  }
 </script>
 
+<UserPanel openned={userPanelOpenned} {author} {therapist} on:closeUserPanel={onToggleUserPanel}
+  on:updateUser={onUpdateUser} />
+
+<header>
+  <Username username={author.name} on:openUserPanel={onToggleUserPanel} />
+</header>
 <main>
-  <h1>Hello world!</h1>
-  <Button unelevated>Material Button</Button>
+  <PatientView {patients} />
 </main>
 
 <style src="./App.scss" global></style>
