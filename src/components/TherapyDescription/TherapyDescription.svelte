c17039af2d8ced29a6d5e42583f45c385050e56d
mlbiche <tmlb@protonmail.com>
2020-04-03 15:54:27 +0200

    Add datatable to TherapyDescription
    - Add datatable to TherapyDescription
    - Add default services

--- src/components/TherapyDescription/TherapyDescription.svelte
+++ src/components/TherapyDescription/TherapyDescription.svelte
@@ -1,7 +1,13 @@
 <script>
   import { afterUpdate } from 'svelte'
+  import { getServiceLightLabel } from '../../js/utils' // eslint-disable-line no-unused-vars
 
-  export let services
+  export let services = [ // eslint-disable-line prefer-const
+    { code: 1200, duration: 5 },
+    { code: 1003, duration: 55 }
+  ]
+
+  let totalDuration
 
   afterUpdate(() => {
     const colorPalette = [
@@ -12,18 +18,16 @@
       '#bbe1a7'
     ]
 
-    const totalDuration = services.reduce((total, service) => total + service.duration, 0)
+    totalDuration = services.reduce((total, service) => total + service.duration, 0)
 
     services.forEach((service, i) => {
       const servicePercentage = (service.duration * 100) / totalDuration
-      const serviceDiv = document.querySelector(`#service-${i}`)
+      const serviceDiv = document.querySelector(`#service-bar-${i}`)
       const randomIndex = Math.floor(Math.random() * colorPalette.length)
 
-      service.color = colorPalette[randomIndex]
-      colorPalette.splice(randomIndex, 1)
       serviceDiv.style.width = `${servicePercentage}%`
-      serviceDiv.style.backgroundColor = service.color
-      document.querySelector(`#service-caption-${i}`).style.backgroundColor = service.color
+      serviceDiv.style.backgroundColor = colorPalette[randomIndex]
+      colorPalette.splice(randomIndex, 1)
     })
   })
 </script>
@@ -31,15 +35,33 @@
 <h2 class="mdc-typography--headline5">Votre séance</h2>
 <div class="services-bar">
   {#each services as service, i}
-    <div id="service-{i}"></div>
+    <div id="service-bar-{i}"></div>
   {/each}
 </div>
-<ul class="services-caption">
-  {#each services as service, i}
-    <li>
-      <div class="service-caption-color" id="service-caption-{i}"></div>{service.code} : {service.duration} minutes
-    </li>
-  {/each}
-</ul>
+<div class="mdc-data-table">
+  <table class="mdc-data-table__table" aria-label="Dessert calories">
+    <thead>
+      <tr class="mdc-data-table__header-row">
+        <th class="mdc-data-table__header-cell" role="columnheader" scope="col">Thérapie</th>
+        <th class="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">
+          Durée</th>
+      </tr>
+    </thead>
+    <tbody class="mdc-data-table__content">
+      {#each services as service, i}
+        <tr class="mdc-data-table__row">
+          <td class="mdc-data-table__cell">{getServiceLightLabel(service.code)}</td>
+          <td class="mdc-data-table__cell mdc-data-table__cell--numeric">{service.duration}</td>
+        </tr>
+      {/each}
+    </tbody>
+    <tfoot class="mdc-data-table__footer-row">
+      <tr>
+        <td class="mdc-data-table__header-cell" role="rowheader">Durée de la séance</td>
+        <td class="mdc-data-table__cell mdc-data-table__cell--numeric total-duration">{totalDuration}</td>
+      </tr>
+    </tfoot>
+  </table>
+</div>
 
 <style src="TherapyDescription.scss"></style>
