41d3d39cdb26f7feace0052ed51b7fbf02797cd3
Mlbiche <tmlb@protonmail.com>
2020-04-06 12:05:46 +0200

    Implement Select component

---
+++ src/components/Select/Select.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { onMount } from 'svelte'
+  import { MDCSelect } from '@material/select'
+
+  // eslint-disable-next-line prefer-const
+  export let options = [{ value: '', label: '' }]
+  export let value = ''
+
+  let select
+  let thisMDCSelect
+
+  onMount(() => {
+    thisMDCSelect = new MDCSelect(select)
+
+    thisMDCSelect.listen('MDCSelect:change', () => {
+      value = thisMDCSelect.value
+    })
+  })
+</script>
+
+<div bind:this={select} class="mdc-select">
+  <div class="mdc-select__anchor select-width">
+    <i class="mdc-select__dropdown-icon"></i>
+    <div class="mdc-select__selected-text"></div>
+    <span class="mdc-floating-label">
+      <slot></slot>
+    </span>
+    <div class="mdc-line-ripple"></div>
+  </div>
+
+  <div class="mdc-select__menu mdc-menu mdc-menu-surface select-width">
+    <ul class="mdc-list">
+      {#each options as option}
+        <li class="mdc-list-item {option.value === value ? 'mdc-list-item--selected' : ''}" data-value={option.value}
+          aria-selected="true">
+          {option.label}
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
+
+<style src="Select.scss"></style>
