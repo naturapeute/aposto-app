a5803886a7a465e581d5938eaeae10d253f14fd4
Mlbiche <tmlb@protonmail.com>
2020-04-08 12:12:27 +0200

    Use touch wrapper for chips

--- src/components/PatientList/PatientList.svelte
+++ src/components/PatientList/PatientList.svelte
@@ -40,14 +40,16 @@
 
 <ul class="mdc-chip-set mdc-chip-set--choice" role="grid">
   {#each bestMatches as patient (patient.id)}
-    <li class="mdc-chip" role="row" on:click={() => onChipClick(patient.id)}>
-      <div class="mdc-chip__ripple"></div>
-      <i class="material-icons-round mdc-chip__icon mdc-chip__icon--leading">face</i>
-      <span role="gridcell">
-        <span role="button" tabindex="0" class="mdc-chip__primary-action">
-          <span class="mdc-chip__text">{patient.firstName} {patient.lastName}</span>
+    <li class="mdc-touch-target-wrapper" on:click={() => onChipClick(patient.id)}>
+      <div class="mdc-chip mdc-chip--touch">
+        <div class="mdc-chip__ripple"></div>
+        <i class="material-icons-round mdc-chip__icon mdc-chip__icon--leading">face</i>
+        <span role="gridcell">
+          <span role="button" tabindex="0" class="mdc-chip__primary-action">
+            <span class="mdc-chip__text">{patient.firstName} {patient.lastName}</span>
+          </span>
         </span>
-      </span>
+      </div>
     </li>
   {/each}
 </ul>
