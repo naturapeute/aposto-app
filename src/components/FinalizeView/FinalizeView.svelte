430da58e31d204697e480bf160e69890c0f8f6be
Mlbiche <tmlb@protonmail.com>
2020-04-10 17:08:22 +0200

    Add confirm dialog to Finalize View

--- src/components/FinalizeView/FinalizeView.svelte
+++ src/components/FinalizeView/FinalizeView.svelte
@@ -1,20 +1,37 @@
 <script>
-  import { servicePrice } from '../../js/store'
+  import { servicePrice, loading } from '../../js/store'
   import FinalizePatient from '../FinalizePatient/FinalizePatient.svelte'
   import FinalizeTherapyDescription
     from '../FinalizeTherapyDescription/FinalizeTherapyDescription.svelte'
   import IconButton from '../IconButton/IconButton.svelte'
+  import FinalizeConfirmDialog from '../FinalizeConfirmDialog/FinalizeConfirmDialog.svelte'
 
   export let patient
   export let services
+
+  let confirmDialog
+  let dontShowAgain = window.localStorage.getItem('dontShowAgainConfirmSend')
 
   $: totalAmount = services.reduce(
     (total, service) => total + (service.duration * $servicePrice / 60),
     0
   )
+
+  const onSendInvoice = () => {
+    if (!dontShowAgain)
+      confirmDialog.open()
+    else
+      onConfirmSend()
+  }
+
+  const onConfirmSend = () => {
+    loading.set(true)
+    document.querySelector('.send-button > *').disabled = true
+    // TODO : Sending
+  }
 </script>
 
-<form>
+<form on:submit|preventDefault={onSendInvoice}>
   <div class="card-set">
     <div class="mdc-card">
       <FinalizePatient {patient} />
@@ -32,10 +49,12 @@
     </div>
   </div>
   <div class="send-button">
-    <IconButton type="submit" title="Valider les informations de la facture" fabLabel="Envoyer" fab>
+    <IconButton type="submit" title="Envoyer la facture par mail au patient" fabLabel="Envoyer" fab>
       send
     </IconButton>
   </div>
 </form>
+
+<FinalizeConfirmDialog bind:this={confirmDialog} bind:dontShowAgain {patient} on:confirm={onConfirmSend} />
 
 <style src="FinalizeView.scss"></style>
