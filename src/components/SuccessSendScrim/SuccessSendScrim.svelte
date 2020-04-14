dadeac27391d5b5a9c00b4e63828e6502a4ce79c
Mlbiche <tmlb@protonmail.com>
2020-04-14 17:10:15 +0200

    Implement send success message with scrimmed background

---
+++ src/components/SuccessSendScrim/SuccessSendScrim.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Button from '../Button/Button.svelte'
+
+  export let patient
+</script>
+
+<div class="success-scrim"></div>
+<div class="success-container">
+  <h1 class="mdc-typography--headline2">C'est envoyé !</h1>
+  <p>
+    Votre nouvelle facture a bien été envoyée à <span
+      class="typography--button-inline no-click-inline">
+      {patient.firstName} {patient.lastName}</span>.
+  </p>
+  <p>Vous devriez recevoir une copie dans votre boîte mail sous peu.</p>
+  <Button>
+    Créer une nouvelle facture
+  </Button>
+</div>
+
+<style src="SuccessSendScrim.scss"></style>
