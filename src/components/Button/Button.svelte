ade3a161acb641b9c8eb00c59f1620ddedea34d7
Mlbiche <tmlb@protonmail.com>
2020-04-14 11:34:31 +0200

    Add dialog button properties

--- src/components/Button/Button.svelte
+++ src/components/Button/Button.svelte
@@ -5,10 +5,16 @@
   export let icon = null
   export let type = 'button'
   export let title = ''
+  export let dialog = false
+  export let dialogAction = null
+  export let dialogInitialFocus = null
+  export let className = ''
 </script>
 
-<button class="mdc-button" class:mdc-button--raised="{raised}"
-  class:mdc-button--unelevated="{unelevated}" class:mdc-button--outlined="{outlined}" {title}
+<button class="mdc-button {className}" class:mdc-button--raised="{raised}"
+  class:mdc-button--unelevated="{unelevated}" class:mdc-button--outlined="{outlined}"
+  class:mdc-dialog__button="{dialog}" data-mdc-dialog-action="{dialogAction}"
+  data-mdc-dialog-initial-focus="{dialogInitialFocus}" {title}
   on:click {type}>
   <div class="mdc-button__ripple"></div>
   {#if icon}
