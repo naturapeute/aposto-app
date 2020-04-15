59aa3559dfb41315c9ca9ebd36bc47d98e694a58
Mlbiche <tmlb@protonmail.com>
2020-04-15 12:31:36 +0200

    Implement Chip component with custom color option

---
+++ src/components/Chip/Chip.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let touchWrapper = false
+  export let leadingIcon = ''
+  export let trailingIcon = ''
+  export let color = ''
+  export let className = ''
+
+  let element
+
+  $: if (element && color) element.style.setProperty('--chip-color', color)
+</script>
+
+<div bind:this={element} class="mdc-chip {className}" class:mdc-chip--touch={touchWrapper}
+  class:colored={color} on:click>
+  <div class="mdc-chip__ripple"></div>
+  {#if leadingIcon}
+    <i class="material-icons-round mdc-chip__icon mdc-chip__icon--leading">
+      {leadingIcon}
+    </i>
+  {/if}
+  <span role="gridcell">
+    <span tabindex="0" class="mdc-chip__primary-action">
+      <span class="mdc-chip__text">
+        <slot></slot>
+      </span>
+    </span>
+  </span>
+  {#if trailingIcon}
+    <span role="gridcell">
+      <i class="material-icons-round mdc-chip__icon mdc-chip__icon--trailing" tabindex="-1"
+        role="button">
+        {trailingIcon}
+      </i>
+    </span>
+  {/if}
+</div>
+
+<style src="Chip.scss"></style>
