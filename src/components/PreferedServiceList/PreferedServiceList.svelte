290ee9848571c4c134a271487b8007a936417e76
Mlbiche <tmlb@protonmail.com>
2020-04-17 20:01:25 +0200

    Move service icon in prefered service list out from chips

--- src/components/PreferedServiceList/PreferedServiceList.svelte
+++ src/components/PreferedServiceList/PreferedServiceList.svelte
@@ -14,13 +14,16 @@
 </script>
 
 <ul class="mdc-chip-set mdc-chip-set--choice" role="grid">
+  <i class="material-icons-round">spa</i>
   {#each $preferedServices as preferedService (preferedService.code)}
     <li class="mdc-touch-target-wrapper" on:click={() => onChipClick(preferedService.code)}>
       <Chip title="Sélectionner le service {`"${getServiceLightLabel(preferedService.code)}"`}"
-        leadingIcon="spa" color="{preferedService.color}"
-        selected={preferedService.code === selectedServiceCode} touchWrapper>
+        color="{preferedService.color}" selected={preferedService.code === selectedServiceCode}
+        touchWrapper>
         {getServiceLightLabel(preferedService.code)}
       </Chip>
     </li>
   {/each}
 </ul>
+
+<style src="PreferedServiceList.scss"></style>
