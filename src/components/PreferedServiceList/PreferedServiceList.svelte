617b847e9766b208ff5186c3b2e69fe9582d196d
Mlbiche <tmlb@protonmail.com>
2020-04-23 15:34:15 +0200

    Make prefered service list more generic to be reused in user panel

--- src/components/PreferedServiceList/PreferedServiceList.svelte
+++ src/components/PreferedServiceList/PreferedServiceList.svelte
@@ -4,32 +4,41 @@
   import { getServiceLightLabel } from '../../js/utils'
   import Chip from '../Chip/Chip.svelte'
 
-  export let selectedServiceCode
+  export let selectedServiceCode = null
 
   let iconElement
   const dispatch = createEventDispatcher()
 
   afterUpdate(() => {
-    const selectedServiceColor = $preferedServices.find(
-      preferedService => preferedService.code === selectedServiceCode
-    ).color
-
-    iconElement.style.setProperty('--selected-service-color', selectedServiceColor)
+    if (selectedServiceCode) {
+      const selectedServiceColor = $preferedServices.find(
+        preferedService => preferedService.code === selectedServiceCode
+      ).color
+  
+      iconElement.style.setProperty('--selected-service-color', selectedServiceColor)
+    }
   })
 
-  function onChipClick(code) {
+  function onSelectService(code) {
     dispatch('selectedService', code)
+  }
+
+  function onDeletePreferedService(code) {
+    $preferedServices = $preferedServices.filter(preferedService => preferedService.code !== code)
   }
 </script>
 
 <ul class="mdc-chip-set mdc-chip-set--choice" role="grid">
-  <i bind:this={iconElement} class="material-icons-outlined">spa</i>
+  {#if selectedServiceCode}
+    <i bind:this={iconElement} class="material-icons-outlined">spa</i>
+  {/if}
   {#each $preferedServices as preferedService (preferedService.code)}
-    <li class="mdc-touch-target-wrapper" on:click={() => onChipClick(preferedService.code)}>
-      <Chip className="prefered-service-chip"
+    <li class="mdc-touch-target-wrapper" on:click={() => onSelectService(preferedService.code)}>
+      <Chip className="{selectedServiceCode ? 'prefered-service-chip' : ''}"
         title="Sélectionner le service {`"${getServiceLightLabel(preferedService.code)}"`}"
         color="{preferedService.color}" selected={preferedService.code === selectedServiceCode}
-        touchWrapper>
+        trailingIcon={selectedServiceCode ? null : 'delete'}
+        on:trailingIconClick={() => onDeletePreferedService(preferedService.code)} touchWrapper>
         {getServiceLightLabel(preferedService.code)}
       </Chip>
     </li>
