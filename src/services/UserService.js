e1024a2df08e45045b96fbe2595292c5d4a91240
Mlbiche <tmlb@protonmail.com>
2020-04-29 12:38:07 +0200

    Implement save user service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,12 +2,52 @@ export async function authenticate(email) {
   const response = await fetch(`https://terrapeute.ch/api/therapist/${email}`, {
     method: 'GET',
     headers: {
-      Accept: 'application/json'
+      Accept: 'application/json',
+      Origin: process.env.APP_URL
     }
   })
 
   if (response.ok)
     return await response.json()
+  else {
+    const body = await response.text()
+
+    throw Error(body)
+  }
+}
+
+export async function saveUser(
+  terrapeuteUserID,
+  author,
+  therapist,
+  servicePrice,
+  preferedServices
+) {
+  const extraData = {
+    author: {
+      name: author.name,
+      RCC: author.RCC,
+      GLN: author.GLN
+    },
+    therapist: {
+      RCC: therapist.RCC,
+      GLN: therapist.GLN
+    },
+    servicePrice: servicePrice,
+    preferedServices: preferedServices.map(e => ({ ...e }))
+  }
+
+  const response = await fetch(`https://terrapeute.ch/api/therapist/${terrapeuteUserID}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Origin: process.env.APP_URL
+    },
+    body: JSON.stringify(extraData)
+  })
+
+  if (response.ok)
+    return await response.text()
   else {
     const body = await response.text()
 
