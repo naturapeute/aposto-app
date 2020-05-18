e95459912938d95b10b98baa9d50a90f8692e290
Vincent Agnano <vinyll@protonmail.com>
2020-05-18 12:31:16 +0200

    getting Matomo to work again besides Svelte

--- src/main.js
+++ src/main.js
@@ -5,25 +5,3 @@ const app = new App({
 })
 
 export default app
-
-let _paq
-loadMatomo()
-
-function loadMatomo() {
-  _paq = window._paq || []
-  _paq.push(['trackPageView'])
-  _paq.push(['enableLinkTracking'])
-  ; (function () {
-    const u = '//stats.anonym.dev/'
-    _paq.push(['setTrackerUrl', u + 'matomo.php'])
-    _paq.push(['setSiteId', '5'])
-    const d = document
-    const g = d.createElement('script')
-    const s = d.getElementsByTagName('script')[0]
-    g.type = 'text/javascript'
-    g.async = true
-    g.defer = true
-    g.src = u + 'matomo.js'
-    s.parentNode.insertBefore(g, s)
-  })()
-}
