dfbbc5b933ffb098d25b6769ee8103e206eeb709
Mlbiche <tmlb@protonmail.com>
2020-06-18 13:40:48 +0200

    Add Material switch

---
+++ src/components/Switch/Switch.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { MDCSwitch } from '@material/switch'
+  import { onMount, onDestroy } from 'svelte'
+
+  export let switchId
+  export let checked = false
+
+  let switchControlElement
+  let switchControl
+
+  onMount(() => {
+    switchControl = new MDCSwitch(switchControlElement)
+  })
+
+  onDestroy(() => {
+    if (switchControlElement)
+      switchControl.destroy()
+  })
+</script>
+
+<div bind:this={switchControlElement} class="mdc-switch">
+  <div class="mdc-switch__track"></div>
+  <div class="mdc-switch__thumb-underlay">
+    <div class="mdc-switch__thumb"></div>
+    <input bind:checked type="checkbox" id={switchId} class="mdc-switch__native-control"
+      role="switch" aria-checked="false">
+  </div>
+</div>
+<label class="switch-label" for={switchId}>
+  <slot></slot>
+</label>
+
+<style src="Switch.scss"></style>
